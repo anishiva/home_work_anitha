f2dbdf39068941e289a058dbee47cbda4b1b3cff
anishiva <anitha@subbu.us>
2015-07-12 20:14:12 -0400

    Calculate
    This Ruby program calculates the sum and product of given two numbers.

---
+++ calculate.rb
@@ -0,0 +1,12 @@
+# Add & Multiply two numbers
+
+puts "Enter a number : "
+a = gets.chomp.to_f
+puts "Enter another number : "
+b = gets.chomp.to_f
+
+sum = a + b
+product = a * b
+
+puts "Sum of #{a} + #{b} = #{sum}"
+puts "Product  #{a} * #{b} = #{product}"
